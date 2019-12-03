29e764ba5e7bb67feb731d6f796f094439441563
VicEspino <taxgype1@gmail.com>
2019-12-02 23:52:06 -0600

    Funciona multiples inicios de sesión sobre la app

--- app/src/main/java/com/example/e_tiket/conexion_red/ConexionServidor.java
+++ app/src/main/java/com/example/e_tiket/conexion_red/ConexionServidor.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 
 import models.Ticket;
@@ -24,22 +25,37 @@ public class ConexionServidor {
             public void run() {
                 try {
                     serverSocket = new ServerSocket(1399);
-                    while(true){
+                    serverSocket.setReuseAddress(true);
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                while(true) {
+                    try {
+
                         //esperara una conexión
                         Socket socket = serverSocket.accept();
                         //DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
                         //String data = dataInputStream.readUTF();
                         ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
                         ArrayList<Ticket> listTickets = (ArrayList<Ticket>) objectInputStream.readObject();
-                        if(putList!=null){
+                        int a = 1;
+                        a++;
+                        if (putList != null) {
                             putList.setAdapter(listTickets);
+                            //mata el hilo
+                            serverSocket.close();
+                            break;
                         }
                         //Toast.makeText(context, "algo recibido", Toast.LENGTH_LONG).show();
+
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
                 }
             }
         });
